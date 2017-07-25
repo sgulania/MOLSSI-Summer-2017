62c2a2160ef1ef2cb84a12eab1b9bb32193d2543
DTS Loaner <dtsloaner@DTSs-MacBook-Pro-2.local>
2017-07-25 15:24:02 -0400

    tested_25July2017

--- tests/test_math.py
+++ tests/test_math.py
@@ -9,5 +9,14 @@ def test_add():
     assert ms.math.add(5,2) == 7
     assert ms.math.add(2,5) == 7
 
+def test_sub():
+    assert ms.math.sub(5,2) == 3
+    assert ms.math.sub(2,5) == -3
 
+def test_mult():
+    assert ms.math.mult(5,2) == 10
+    assert ms.math.mult(2,5) == 10
 
+def test_div():
+    assert ms.math.div(4,2) == 2
+    assert ms.math.div(6,2) == 3
