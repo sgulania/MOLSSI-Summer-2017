4c8e4a6cac96f70346f8df38b0881bc694cb6913
DTS Loaner <dtsloaner@DTSs-MacBook-Pro-2.local>
2017-07-25 14:29:00 -0400

    math module

---
+++ MOLSSI_SUMMER_2017/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the base file of the MOLSSI Summer 2017
+"""
+
+from . import math
+
---
+++ MOLSSI_SUMMER_2017/math.py
@@ -0,0 +1,31 @@
+"""
+A smal set of function for doing math operations.
+"""
+
+# Write a function that add two values
+def add(a,b):
+    """
+    Function that adds two numbers
+    """
+    return a + b
+
+# Write a function that multiply two values
+def mult(a,b):
+    """
+    Function that multiply two numbers
+    """
+    return a * b
+
+# Write a function that subtract two values
+def sub(a,b):
+    """
+    Function that subtract two numbers
+    """
+    return a - b
+
+# Write a function that divide two values
+def div(a,b):
+    """
+    Function that divide two numbers
+    """
+    return a / b
